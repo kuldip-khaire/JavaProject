3d348e177f6f9ab0a94afc43aae4e1d90036d181
Kuldip <kuldipkhaire27@gmail.com>
2022-12-24 17:45:39 +0530

    Second commit : added for LinkedListDemo

---
+++ src/com/velocity/demo1/LinkedListDemo.java
@@ -0,0 +1,18 @@
+package com.velocity.demo1;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+	
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		
+		System.out.println(list);
+		
+	}
+
+}
