a3c85d545455bdaf6909de4a410c2ac9ef837e09
Kuldip <kuldipkhaire27@gmail.com>
2022-12-24 17:18:14 +0530

    first commit : added ArrayListDemo code

---
+++ src/com/velocity/demo1/ArrayListDemo.java
@@ -0,0 +1,19 @@
+package com.velocity.demo1;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		list.add("Kshitija");
+		list.add("Kuldip");
+		list.add("Khaire");
+		
+		System.out.println(list);
+
+	}
+
+}
