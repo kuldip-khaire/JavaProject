16b051152470a547b25296e53dffbbbf5e052d74
Kuldip <kuldipkhaire27@gmail.com>
2022-12-26 13:55:48 +0530

    syso2 is added

--- src/com/velocity/demo1/demo1.java
+++ src/com/velocity/demo1/demo1.java
@@ -1,5 +1,8 @@
-public class demo{
+package com.velocity.demo1;
+
+public class demo1{
 public static void main(String[] args) {
 System.out.println("demo");
+System.out.println("Dev-02 in sceanario -02 ");
 }
 }
